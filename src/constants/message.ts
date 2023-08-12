8d68084e1847f025d65a4f7d57fe1682d505ba02
Nadhirah Mustafa <nadhirahmustafa5@gmail.com>
2023-08-13 03:25:48 +0800

    fix message

--- src/constants/message.ts
+++ src/constants/message.ts
@@ -1,7 +1,7 @@
 export const dashboard = {
     title: 'Movie dashboard',
     no_result: 'Sorry, no result found!',
-    error_message: 'You may filter your result based on the title, genres, year, runtime, director or actors in the search box above',
+    error_message: 'You may filter your result based on the title in the search box above',
     result_found: ' result(s) found',
     now_playing: 'Now Playing',
     top_rated: 'Top Rated',
